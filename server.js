0e5f6976551962f28d5172fd87b9a6a29933faa4
LogicX1 <uninajone@gmail.com>
2020-01-19 12:22:35 +0200

    handler created

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 var http = require('http');
 
-var server = http.createServer();
+
+var message = ' Me is happy me is doing workshop ';
+
+function handler (request , response) {
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.write(message);
+    response.end();
+}
+
+
+
+
+
+
+
+
+
+var server = http.createServer(handler);
 
 server.listen(5050 , () => {
     console.log('Server is listening on port 5050,armed and ready!');
