130d2bfe1719e5c1ad30a15c8ef3d02ba1640f26
LogicX1 <uninajone@gmail.com>
2020-01-19 14:22:57 +0200

    geric extensions

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-
+const path = require('path');
 var message = ' Me is happy me is doing workshop ';
 
 function handler (request , response) {
@@ -31,13 +31,36 @@ function handler (request , response) {
         response.end();
 
     }else{
-    response.writeHead(404,{'Content-Type':'text/html'});
-    response.write('Not Found!');
-    response.end();
+        var extension = endpoint.split('.')[1];
+        var extensionType = {
+            html:'text/html',
+            css:'text/css',
+            js:'application/javascript',
+            ico:'image/x-icon',
+            jpg:'image/x-icon',
+            png:'image/x-icon' 
+        }
+        
+        const filePath = path.join(__dirname ,'/public/',endpoint)
+        console.log(filePath)
+        fs.readFile(filePath,(error,file)=> {
+            if(error){
+                console.log(error);
+                response.writeHead(500);
+                response.end('Cant handle this!')
+            }else {
+
+                response.writeHead(200,{'Content-Type':extensionType[extension]});
+                response.end(file);
+            }
+            
+        }); 
     }
 }
 
-
+// response.writeHead(404,{'Content-Type':'text/html'});
+//     response.write('Not Found!');
+//     response.end();
 
 
 
