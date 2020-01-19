3675ebac646f7c06bd182967de5a6c3c8e1bb9fa
LogicX1 <uninajone@gmail.com>
2020-01-19 12:33:54 +0200

    Serving index.html

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 var http = require('http');
-
+var fs = require('fs');
 
 var message = ' Me is happy me is doing workshop ';
 
 function handler (request , response) {
-    response.writeHead(200,{'Content-Type':'text/html'});
-    response.write(message);
+    var endpoint = request.url;
+    console.log(endpoint);
+    var method = request.method;
+    console.log(method);
+
+
+    if(endpoint === '/'){
+        response.writeHead(200,{'Content-Type':'text/html'});
+        fs.readFile(__dirname +'/public/index.html',(error,file)=> {
+            if(error){
+                console.log('Error!')
+                return;
+            }
+            response.end(file);
+        });
+    }
+    else if(endpoint === '/node'){
+        response.writeHead(200,{'Content-Type':'text/html'});
+        response.write('Welcome to node page');
+        response.end();
+
+    }else if (endpoint === '/girls'){
+        response.writeHead(200,{'Content-Type':'text/html'});
+        response.write('Welcome to girls page');
+        response.end();
+
+    }else{
+    response.writeHead(404,{'Content-Type':'text/html'});
+    response.write('Not Found!');
     response.end();
+    }
 }
 
 
